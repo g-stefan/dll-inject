b6bfa3dede4e921e790961a05c9d344d89c3dad6
Grigore Stefan <g_stefan@yahoo.com>
2023-01-09 16:18:20 +0200

    Update

--- source/XYO/DllInject/Dependency.hpp
+++ source/XYO/DllInject/Dependency.hpp
@@ -1,7 +1,7 @@
 // Dll Inject
 // Copyright (c) 2008-2023 Grigore Stefan <g_stefan@yahoo.com>
 // MIT License (MIT) <http://opensource.org/licenses/MIT>
-// SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-FileCopyrightText: 2008-2023 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: MIT
 
 #ifndef XYO_DLLINJECT_DEPENDENCY_HPP
