bc9dd1a5827a76fa99099ec560b26d1415eba6a4
Grigore Stefan <g_stefan@yahoo.com>
2024-01-02 22:49:13 +0200

    Update

--- source/XYO/DllInject/Version.hpp
+++ source/XYO/DllInject/Version.hpp
@@ -1,7 +1,7 @@
 // Dll Inject
-// Copyright (c) 2008-2023 Grigore Stefan <g_stefan@yahoo.com>
+// Copyright (c) 2008-2024 Grigore Stefan <g_stefan@yahoo.com>
 // MIT License (MIT) <http://opensource.org/licenses/MIT>
-// SPDX-FileCopyrightText: 2008-2023 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-FileCopyrightText: 2008-2024 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: MIT
 
 #ifndef XYO_DLLINJECT_VERSION_HPP
